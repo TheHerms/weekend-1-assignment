8d69f1b40339a482bd6302c3536684506f9ee045
Tyler Herman <tylerherman@Tylers-MacBook-Pro.local>
2017-01-09 09:04:03 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var totalSalary = 0;
+
 $(function () {
   console.log('document is ready');
 
@@ -16,18 +18,55 @@ $(function () {
     appendDom(formData);
 
     clearForm();
+
   });
+
+/*$('#employeeRemoval').on('submit',function (event) {
+
+    event.preventDefault();
+
+    var employeeRemoval = event;
+
+    console.log("Remove " + employeeRemove);
+
+    clearForm();
+  })*/
+
 });
 
 function appendDom(emp) {
   var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  var $empName = $('<div class="column" id="empFirtstName"> ' + emp.employeeFirstName + ' ' + emp.employeeLastName +' </div>');
 
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
+  var $empID = $('<div class="column" id="empID">' + emp.employeeIdNumber +'</div>');
+  var $empJobTitle = $('<div class="column" id="empJobTitle">' + emp.JobTitle + '</div>');
+  var $empAnnualSalary = $('<div class="column annualSalaryClass" id="empAnnualSalary">' + emp.employeeSalary +'</div>');
+  totalSalary += emp.employeeSalary;
+
+  $emp.append($empName); // add our employee data
+  $emp.append($empID);
+  $emp.append($empJobTitle);
+  $emp.append($empAnnualSalary);
 
   $('#employees').append($emp); // append our div to the DOM
+  calculateSalary();
+
+}
+
+function calculateSalary() {
+
+  var $totalMonthlySalary = totalSalary/12;
+
+  $('div').remove('.monthly');
+  var $exp = $('<div class="monthly"></div>');
+  $($exp).append('<p>' + $totalMonthlySalary + '</p>');
+  $($exp).appendTo('#monthly');
+  console.log($totalMonthlySalary);
+
+
 }
 
 function clearForm() {
   $('form').find('input[type=text]').val('');
+  $('form').find('input[type=number]').val('');
 }
